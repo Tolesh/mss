f41f8fdcc509b7eec24be42a5a0a945a1dde9052
Tolesh <57959682+Tolesh@users.noreply.github.com>
2021-05-06 15:46:26 +0600

    HistoryScreen Update 06.05.21
    HistoryScreen Update 06.05.21

--- Procat final/procat/views/HistoryScreen.js
+++ Procat final/procat/views/HistoryScreen.js
@@ -1,14 +1,95 @@
 import React, { useContext } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import PropTypes from 'prop-types';
-import { StyleSheet, Text, View, Image, Pressable, TextInput, Button } from 'react-native';
+import { StyleSheet, Text, View, Image, Pressable, TextInput, Button,Dimensions } from 'react-native';
 import { CheckBox } from 'react-native';
 
+const GLOBAL = require('../views/Globals');
+const getUsersUrl = GLOBAL.BASE_URL + 'HistoryScreen.php?action=get_users&lang=1';
+const valuesJsonUrl = GLOBAL.BASE_URL + 'values.php?action=get_values&lang=1';
 
+const dimensions = Dimensions.get('window');
+const windowHeight = dimensions.height;
+const Cars = (props) => {
+    return(
+        <View style={styles.content}>
+            <View style={styles.image_4}>
+                <Image source={require('../images/image_1.jpg')}></Image> 
+            </View>
+            <View style={styles.info}>
+                <Text style={styles.car_name}>{props.name}</Text>
+                <Text style={styles.car_info}>{props.godVypuska}</Text>
+                <Text style={styles.car_info}>{props.koropka}</Text>
+                <Text style={styles.car_info}>{props.color}</Text>
+                <Text style={styles.car_info}>{props.kolichMest}</Text>
+            </View>
+            <View style={styles.price}>
+                <Image style={styles.price_text} source={require('../images/logo.png')}></Image>
+                <Text style={styles.price_text}>{props.priceSutki}</Text>
+            </View>
+        </View>
+    )
+}
 class History extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    state = {
+        data: [],
+        values: [],
+        search: '',
+    }
+
+    arrayholder = [];
+
+    componentDidMount = async () => {
+        const values_response = await fetch(valuesJsonUrl);
+        const values = await values_response.json();
+
+        this.setState({ values });
+
+        this.getUsers();
+    }
+
+    getUsers = () => {
+        var json = '{"targets": "' + GLOBAL.SERVER_RESULT + '"}';
+        const request = new Request(getUsersUrl, { method: 'POST', body: json });
+
+        fetch(request)
+            .then(response => {
+                if (response.status === 200) {
+                    return response.json();
+                } else {
+                    throw new Error('Something went wrong on api server!');
+                }
+            })
+            .then(response => {
+                this.setState({ data: response });
+                this.arrayholder = response;
+            }).catch(error => {
+                console.error(error);
+            });
+    }
+
+    searchFilterFunction = (text) => {
+        const newData = this.arrayholder.filter(item => {
+            const itemData = `${item.name.toUpperCase()} ${item.lastname.toUpperCase()}`;
+            const textData = text.toUpperCase();
+            
+            return itemData.indexOf(textData) > -1;
+        });
+
+        this.setState({ data: newData });
+        this.setState({ search: text });
+    };
 
     render(){
-
+        let menu = new Menu();
+        let { data, search } = this.state;
+        let CarsData = [{id : data.id,name: data.name,godVypuska: data.god_vipuska,koropka: data.korobka,color: data.color,kolichMest: data.mesta,priceSutki: data.price_day}]
+        // ,{id : 2,name: 'Toyota Camry XV70',godVypuska: 'Год выпуска: 2020 г',koropka: 'Коробка: Автомат',color: 'Цвет: белый',kolichMest: 'Кол-во мест: 4',priceSutki: '45 000тг/сутки'},{id : 3,name: 'Hyundai Accent',godVypuska: 'Год выпуска: 2018 г',koropka: 'Коробка: Автомат',color: 'Цвет: белый',kolichMest: 'Кол-во мест: 4',priceSutki: '15 000тг/сутки'}
+        let CarsElements = CarsData.map( (cars) => <Cars name={cars.name} godVypuska={cars.godVypuska} koropka={cars.koropka} color={cars.color} kolichMest={cars.kolichMest} priceSutki={cars.priceSutki}/>);
         return (
             <View style={styles.container}>
                 <Image style={styles.image_3} source={require('../images/Group.png')} />
@@ -17,7 +98,7 @@ class History extends React.Component {
                     </View>
                 </View>
                 <Text style={styles.text3}>История заказов</Text>
-                <View style={styles.content}>
+                {/* <View style={styles.content}>
                     <View style={styles.image_4}>
                         <Image source={require('../images/image_1.jpg')}></Image>
                     </View>
@@ -32,8 +113,11 @@ class History extends React.Component {
                             <Image style={styles.price_text} source={require('../images/logo.png')}></Image>
                             <Text style={styles.price_text}>15 000тг/сутки</Text>
                     </View>
-                </View>
-                <View style={styles.content}>
+                </View> */}
+                {
+                    CarsElements
+                }
+                {/* <View style={styles.content}>
                     <View style={styles.image_4}>
                         <Image source={require('../images/car_2.png')}></Image>
                     </View>
@@ -48,7 +132,7 @@ class History extends React.Component {
                             <Image style={styles.price_text2} source={require('../images/logo.png')}></Image>
                             <Text style={styles.price_text2}>45 000тг/сутки</Text>
                     </View>
-                </View>
+                </View> */}
                 <View style={styles.buttons}>
                     <Button
                         title="Найти машину"
@@ -73,7 +157,7 @@ const styles = StyleSheet.create({
         // flexDirection: 'row', 
         // justifyContent: 'space-between', 
         alignItems: 'center', 
-        marginTop: 184,
+        marginTop: 0,
     },
     header: {
         // borderWidth: 1,
@@ -183,12 +267,12 @@ const styles = StyleSheet.create({
         fontWeight: 'bold',
         // paddingLeft: 40
     },
-    price_text2: {
-        color: 'black',
-        fontSize: 12,
-        marginTop: 8,
-        fontWeight: 'bold'
-    },
+    // price_text2: {
+    //     color: 'black',
+    //     fontSize: 12,
+    //     marginTop: 8,
+    //     fontWeight: 'bold'
+    // },
 });
 
 export default History;
