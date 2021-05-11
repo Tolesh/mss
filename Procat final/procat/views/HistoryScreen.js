e2b15b31619612890e43f6b12ed773a61e2a7a7b
Tolesh <57959682+Tolesh@users.noreply.github.com>
2021-05-11 11:12:25 +0600

    update 11.05.21
    Tolesh

--- Procat final/procat/views/HistoryScreen.js
+++ Procat final/procat/views/HistoryScreen.js
@@ -1,35 +1,16 @@
 import React, { useContext } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import PropTypes from 'prop-types';
-import { StyleSheet, Text, View, Image, Pressable, TextInput, Button,Dimensions } from 'react-native';
+import { StyleSheet, Text, View, Image, Pressable, TextInput, Button,Dimensions, ScrollView, FlatList, SafeAreaView } from 'react-native';
 import { CheckBox } from 'react-native';
 
 const GLOBAL = require('../views/Globals');
-const getUsersUrl = GLOBAL.BASE_URL + 'HistoryScreen.php?action=get_users&lang=1';
+const getUsersUrl = GLOBAL.BASE_URL + 'HistoryScreen.php?action=get_cars&lang=1';
 const valuesJsonUrl = GLOBAL.BASE_URL + 'values.php?action=get_values&lang=1';
 
 const dimensions = Dimensions.get('window');
 const windowHeight = dimensions.height;
-const Cars = (props) => {
-    return(
-        <View style={styles.content}>
-            <View style={styles.image_4}>
-                <Image source={require('../images/image_1.jpg')}></Image> 
-            </View>
-            <View style={styles.info}>
-                <Text style={styles.car_name}>{props.name}</Text>
-                <Text style={styles.car_info}>{props.godVypuska}</Text>
-                <Text style={styles.car_info}>{props.koropka}</Text>
-                <Text style={styles.car_info}>{props.color}</Text>
-                <Text style={styles.car_info}>{props.kolichMest}</Text>
-            </View>
-            <View style={styles.price}>
-                <Image style={styles.price_text} source={require('../images/logo.png')}></Image>
-                <Text style={styles.price_text}>{props.priceSutki}</Text>
-            </View>
-        </View>
-    )
-}
+
 class History extends React.Component {
     constructor(props) {
         super(props);
@@ -55,7 +36,7 @@ class History extends React.Component {
     getUsers = () => {
         var json = '{"targets": "' + GLOBAL.SERVER_RESULT + '"}';
         const request = new Request(getUsersUrl, { method: 'POST', body: json });
-
+        console.log(json);
         fetch(request)
             .then(response => {
                 if (response.status === 200) {
@@ -85,11 +66,7 @@ class History extends React.Component {
     };
 
     render(){
-        let menu = new Menu();
         let { data, search } = this.state;
-        let CarsData = [{id : data.id,name: data.name,godVypuska: data.god_vipuska,koropka: data.korobka,color: data.color,kolichMest: data.mesta,priceSutki: data.price_day}]
-        // ,{id : 2,name: 'Toyota Camry XV70',godVypuska: 'Год выпуска: 2020 г',koropka: 'Коробка: Автомат',color: 'Цвет: белый',kolichMest: 'Кол-во мест: 4',priceSutki: '45 000тг/сутки'},{id : 3,name: 'Hyundai Accent',godVypuska: 'Год выпуска: 2018 г',koropka: 'Коробка: Автомат',color: 'Цвет: белый',kolichMest: 'Кол-во мест: 4',priceSutki: '15 000тг/сутки'}
-        let CarsElements = CarsData.map( (cars) => <Cars name={cars.name} godVypuska={cars.godVypuska} koropka={cars.koropka} color={cars.color} kolichMest={cars.kolichMest} priceSutki={cars.priceSutki}/>);
         return (
             <View style={styles.container}>
                 <Image style={styles.image_3} source={require('../images/Group.png')} />
@@ -98,47 +75,37 @@ class History extends React.Component {
                     </View>
                 </View>
                 <Text style={styles.text3}>История заказов</Text>
-                {/* <View style={styles.content}>
-                    <View style={styles.image_4}>
-                        <Image source={require('../images/image_1.jpg')}></Image>
-                    </View>
-                    <View style={styles.info}>
-                        <Text style={styles.car_name}>Hyundai Accent</Text>
-                        <Text style={styles.car_info}>Год выпуска: 2018 г</Text>
-                        <Text style={styles.car_info}>Коробка: Автомат</Text>
-                        <Text style={styles.car_info}>Цвет: белый</Text>
-                        <Text style={styles.car_info}>Кол-во мест: 4</Text>
-                    </View>
-                    <View style={styles.price}>
-                            <Image style={styles.price_text} source={require('../images/logo.png')}></Image>
-                            <Text style={styles.price_text}>15 000тг/сутки</Text>
-                    </View>
-                </View> */}
-                {
-                    CarsElements
-                }
-                {/* <View style={styles.content}>
-                    <View style={styles.image_4}>
-                        <Image source={require('../images/car_2.png')}></Image>
-                    </View>
-                    <View style={styles.info}>
-                        <Text style={styles.car_name}>Toyota Camry XV70</Text>
-                        <Text style={styles.car_info}>Год выпуска: 2020 г</Text>
-                        <Text style={styles.car_info}>Коробка: Автомат</Text>
-                        <Text style={styles.car_info}>Цвет: белый</Text>
-                        <Text style={styles.car_info}>Кол-во мест: 4</Text>
-                    </View>
-                    <View style={styles.price2}>
-                            <Image style={styles.price_text2} source={require('../images/logo.png')}></Image>
-                            <Text style={styles.price_text2}>45 000тг/сутки</Text>
-                    </View>
-                </View> */}
+                <SafeAreaView scrollEnabled={true}>
+                    <FlatList 
+                        data={this.state.data}
+                        renderItem={({ item }) => (
+                            <View style={styles.content}>
+                                 <View style={styles.image_4}>
+                                    <Image source={require('../images/image_1.jpg')}></Image> 
+                                </View>
+                                <View style={styles.info}>
+                                    <Text style={styles.car_name}>{item.name}</Text>
+                                    <Text style={styles.car_info}>Год выпуска: {item.god_vipuska} г</Text>
+                                    <Text style={styles.car_info}>Коробка: {item.korobka}</Text>
+                                    <Text style={styles.car_info}>Цвет: {item.color}</Text>
+                                    <Text style={styles.car_info}>Кол-во мест: {item.mesta}</Text>
+                                </View>
+                                <View style={styles.price}>
+                                    <Image style={styles.price_text} source={require('../images/logo.png')}></Image>
+                                    <Text style={styles.price_text}>{item.price_day}/сутки</Text>
+                                </View>
+                            </View>
+                        )}
+                    />
+                </SafeAreaView>
                 <View style={styles.buttons}>
                     <Button
                         title="Найти машину"
                         color="#000"
+                        
                     /> 
                 </View>
+                
             </View>
         );
     }
@@ -150,18 +117,13 @@ const styles = StyleSheet.create({
         paddingTop: 70,
         paddingLeft: 21,
         backgroundColor: 'white',
-        // justifyContent: 'center',
-        // alignItems: 'center',
       },
     buttons: {
-        // flexDirection: 'row', 
-        // justifyContent: 'space-between', 
         alignItems: 'center', 
-        marginTop: 0,
+        marginTop: 20
+        
     },
     header: {
-        // borderWidth: 1,
-        // paddingBottom: 40,
         marginBottom: 20,
     },
     block: {
